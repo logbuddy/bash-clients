d03f9e5af380315f4ebadf4f7ced537baff47615
Manuel Kießling <manuel@kiessling.net>
2021-03-21 12:48:04 +0100

    README

--- src/install.sh
+++ src/install.sh
@@ -3,10 +3,10 @@
 mkdir -p "$HOME/serverlogger/bash-clients/includes"
 
 pushd "$HOME/serverlogger/bash-clients/includes" || exit 1
-  curl -O https://raw.githubusercontent.com/serverlogger/bash-clients/main/src/includes/colors.sh
+  curl -s -O https://raw.githubusercontent.com/serverlogger/bash-clients/main/src/includes/colors.sh
   pushd .. || exit 1
-    curl -O https://raw.githubusercontent.com/serverlogger/bash-clients/main/src/setup.sh
-    curl -O https://raw.githubusercontent.com/serverlogger/bash-clients/main/src/streamfile.sh
+    curl -s -O https://raw.githubusercontent.com/serverlogger/bash-clients/main/src/setup.sh
+    curl -s -O https://raw.githubusercontent.com/serverlogger/bash-clients/main/src/streamfile.sh
   popd
 popd || exit 1
 
