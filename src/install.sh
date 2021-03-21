3b65225d088e2de70ed404adf8fd02b7ab872edb
Manuel Kießling <manuel@kiessling.net>
2021-03-21 12:54:36 +0100

    Improvements

--- src/install.sh
+++ src/install.sh
@@ -2,12 +2,12 @@
 
 mkdir -p "$HOME/bin/serverlogger/bash-clients/includes"
 
-pushd "$HOME/bin/serverlogger/bash-clients/includes" || exit 1
+pushd "$HOME/bin/serverlogger/bash-clients/includes" >/dev/null || exit 1
   curl --silent -O https://raw.githubusercontent.com/serverlogger/bash-clients/main/src/includes/colors.sh
-  pushd .. || exit 1
+  pushd .. >/dev/null || exit 1
     curl --silent -O https://raw.githubusercontent.com/serverlogger/bash-clients/main/src/setup.sh
     curl --silent -O https://raw.githubusercontent.com/serverlogger/bash-clients/main/src/streamfile.sh
-  popd
-popd || exit 1
+  popd >/dev/null || exit 1
+popd >/dev/null || exit 1
 
 /usr/bin/env bash "$HOME/bin/serverlogger/bash-clients/setup.sh"
